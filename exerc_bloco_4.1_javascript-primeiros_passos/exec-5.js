1ad9e159e9320cc9e5f1f58c854c98cd4bbfaf82
Jean Machado <jeanvictor.arq@gmail.com>
2020-07-14 22:37:49 -0300

    exercicio 5

---
+++ exerc_bloco_4.1_javascript-primeiros_passos/exec-5.js
@@ -0,0 +1,10 @@
+let angulo1 = 70;
+let angulo2 = 50;
+let angulo3 = 50;
+let somaAngulos = 0;
+
+somaAngulos = angulo1 + angulo2 + angulo3;
+
+if (somaAngulos === 180) {
+    console.log(true)
+} else console.log(false);
---
+++ exerc_bloco_4.1_javascript-primeiros_passos/exerc-4.js
@@ -0,0 +1,7 @@
+let num1 = 0
+
+if (num1 > 0) {
+    console.log("Numero positivo")
+} else if (num1 < 0) {
+    console.log("Numero negativo")
+}else console.log("Zero");
