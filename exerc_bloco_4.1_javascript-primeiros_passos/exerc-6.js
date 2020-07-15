d2b6b9ac03fe6842e78b0d3bac6c3b3e33f23769
Jean Machado <jeanvictor.arq@gmail.com>
2020-07-14 23:02:38 -0300

    exercicio 6

---
+++ exerc_bloco_4.1_javascript-primeiros_passos/exerc-6.js
@@ -0,0 +1,25 @@
+var chessPiece = 'bispo';
+
+switch (chessPiece.toLowerCase()) {
+  case 'rei':
+    console.log('Rei-> Uma casa apenas para qualquer direção.');
+    break;
+  case 'bispo':
+    console.log('Bispo-> Diagonal.');
+    break;
+  case 'rainha':
+    console.log('Rainha-> Diagonal e horizontal.');
+    break;
+  case 'cavalo':
+    console.log('Cavalo -> "L" pode pular sobre as peças.');
+    break;
+  case 'torre':
+    console.log('Torre -> Horizontal.');
+    break;
+  case 'peão':
+    console.log("Peão -> Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+    break;
+  default:
+    console.log('Erro, peça inválida!');
+    break;
+};
