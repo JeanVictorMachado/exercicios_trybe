94f54eb98c6bd31654e7dec78e6c61964ec0a121
Jean Machado <jeanvictor.arq@gmail.com>
2020-07-14 22:21:03 -0300

    exercicio 3

---
+++ exerc_bloco_4.1_javascript-primeiros_passos/exerc-3.js
@@ -0,0 +1,9 @@
+let numero1 = 50
+let numero2 = 100
+let numero3 = 30
+
+if (numero1 > numero2 && numero1 > numero3) {
+    console.log(numero1)
+} else if (numero2 > numero1 && numero2 > numero3) {
+    console.log(numero2);
+} else console.log(numero3);
