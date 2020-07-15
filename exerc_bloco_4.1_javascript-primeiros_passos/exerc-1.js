d67a61589877a5a12bffb2982dfe87b729684bf0
Jean Machado <jeanvictor.arq@gmail.com>
2020-07-14 22:01:09 -0300

    exercicios 1

---
+++ exerc_bloco_4.1_javascript-primeiros_passos/exerc-1.js
@@ -0,0 +1,25 @@
+//let numero1 = 5;
+//let numero2 = 20;
+//let soma = numero1 + numero2;
+//console.log(soma);
+
+//let numero1 = 10;
+//let numero2 = 7;
+//let soma = numero1 - numero2;
+//console.log(soma);
+
+//let numero1 = 10;
+//let numero2 = 7;
+//let soma = numero1 * numero2;
+//console.log(soma);
+
+//let numero1 = 10;
+//let numero2 = 2;
+//let soma = numero1 / numero2;
+//console.log(soma);
+
+//let numero1 = 10;
+//let numero2 = 5;
+//let soma = numero1 % numero2;
+//console.log(soma);
+
