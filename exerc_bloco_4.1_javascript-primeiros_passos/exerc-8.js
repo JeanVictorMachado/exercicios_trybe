0d0c0a72030562896879296708317bf2e7960bb6
Jean Machado <jeanvictor.arq@gmail.com>
2020-07-14 23:36:42 -0300

    exercicio 8

---
+++ exerc_bloco_4.1_javascript-primeiros_passos/exerc-8.js
@@ -0,0 +1,11 @@
+let num1 = 15;
+let num2 = 10;
+let num3 = 13;
+
+let numero = false;
+
+if ((num1 % 2 === 0) || (num2 % 2 === 0) || (num3 % 2 === 0)) {
+    numero = true
+}
+
+console.log(numero);
