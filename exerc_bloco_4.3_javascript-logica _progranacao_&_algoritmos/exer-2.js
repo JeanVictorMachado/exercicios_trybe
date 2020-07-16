fa93dfafd4d13d7c0473e32f671671f4c2bbf4c6
Jean Machado <jeanvictor.arq@gmail.com>
2020-07-15 23:58:21 -0300

    exercicio 2

--- exerc_bloco_4.3_javascript-logica _progranacao_&_algoritmos/exer-2.js
+++ exerc_bloco_4.3_javascript-logica _progranacao_&_algoritmos/exer-2.js
@@ -1,1 +1,16 @@
-console.log("* ");
+//2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+let numero = 5;
+let asteriscos = "* "
+let quadrado = " ";
+
+
+
+for (let i = 0; i <= numero; i += 1) {
+    console.log(quadrado)
+    quadrado = quadrado + asteriscos;
+}
+
+//for (let i = 0; i < 5; i += 1) {
+    //console.log(quadrado);
+//}
