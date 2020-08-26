a033daf18533111514436d15a4b7eba057a16416
Jean Machado <jeanvictor.arq@gmail.com>
2020-08-26 19:36:10 -0300

    exercicios bloco 9.1

---
+++ exerc_bloco_9.1_JavaScript_Assíncrono_e_Callbacks/exemplos-js-assincrono/exemplo-1.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+assert.deepEqual(numbers, [1, 2, 3]);
+
+
+const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+assert.deepEqual(numbers, [1, 2, 3]); // essa validação falha
---
+++ exerc_bloco_9.1_JavaScript_Assíncrono_e_Callbacks/exercicios/exerc-4.js
@@ -0,0 +1,26 @@
+// Suponha que você precise simular uma mensagem enviada do robô Curiosity de Marte para a Terra. O Curiosity envia para a Terra a temperatura atual em Marte, gastando um tempo variável de até 5 segundos para que termine o envio. Crie a função sendMarsTemperature, que imprime a temperatura em Marte.
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
+
+const sendMarsTemperature = (onSuccess, onError) => {
+    const currentTemperature = getMarsTemperature();
+    const messageSuccessfullySent = Math.random() <= 0.6;
+    setTimeout(() => {
+        if(messageSuccessfullySent) onSuccess(currentTemperature)
+        else onError('Robot is busy');
+    }, messageDelay());
+  }
+
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+sendMarsTemperature(greet, handleError);
